bcb4ff09da84317a40a6f579a0eb221693efa7bc
越努力越幸运 <2297819080@qq.com>
2022-04-16 23:49:51 +0800

    完善排序算法之BubbleSort--添加时长测试

--- src/排序算法/BubbleSort.java
+++ src/排序算法/BubbleSort.java
@@ -1,15 +1,34 @@
 package 排序算法;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.Date;
 
 public class BubbleSort {
     public static void main(String[] args) {
-        int arr[] = {3, 9, -1, 10, 20};
-//        int arr[] = {3, 9, -1, 10, -2};
+//        int arr[] = {3, 9, -1, 10, 20};
+////        int arr[] = {3, 9, -1, 10, -2};
+        int[] arr = new int[80000];
+        for(int i =0; i<80000; i++){
+            arr[i] = (int)(Math.random() * 8000000);
+        }
+        Date date1 = new Date();
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String date1Str = simpleDateFormat.format(date1);
+        System.out.println("排序前的时间为："+date1Str);
 
+        bubbleSort(arr);
+        
+        Date date2 = new Date();
+        String date2Str = simpleDateFormat.format(date2);
+        System.out.println("排序前的时间为："+date2Str);
+
+
+    }
+    public static void bubbleSort(int[] arr){
         boolean flag = false;
-        for (int i = 0; i < arr.length - 1; i++){
-            for (int j = 0; j < arr.length - 1 -i; j++) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - 1 - i; j++) {
                 if (arr[j] > arr[j + 1]) {
                     flag = true;
                     int temp = arr[j];
@@ -17,15 +36,14 @@ public class BubbleSort {
                     arr[j + 1] = temp;
                 }
             }
-            System.out.println("第"+(i+1)+"趟排序后的数组");
-            System.out.println(Arrays.toString(arr));
+//            System.out.println("第" + (i + 1) + "趟排序后的数组");
+//            System.out.println(Arrays.toString(arr));
 
-            if(!flag){  //在一趟排序中，一次交换都没有发生过
+            if (!flag) {  //在一趟排序中，一次交换都没有发生过
                 break;
-            }else{
+            } else {
                 flag = false;   //重置flag，进行下一次判断
             }
         }
-
     }
 }
