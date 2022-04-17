127ea5d7061607573e9850e7a0b8439f044d421b
越努力越幸运 <2297819080@qq.com>
2022-04-17 14:18:24 +0800

    添加选择排序算法

---
+++ src/排序算法/InsertSort.java
@@ -0,0 +1,30 @@
+package 排序算法;
+
+import java.util.Arrays;
+
+public class InsertSort {
+    public static void main(String[] args) {
+        int[] arr = {101, 34, 119, 1, -1, 89};
+//      int[] arr = {101, 34, 119, 35, 89};
+        insertSort(arr);
+    }
+
+    //插入排序
+    public static void insertSort(int[] arr) {
+
+        for (int i = 1; i < arr.length; i++) {
+            int insertVal = arr[i];
+            int insertIndex = i-1;
+
+            while (insertIndex >= 0 && insertVal < arr[insertIndex]) {
+                arr[insertIndex + 1] = arr[insertIndex];
+                insertIndex--;
+            }
+            //当退出while循环时，说明插入位置找到:insertIndex+1
+            arr[insertIndex + 1] = insertVal;
+
+            System.out.printf("第轮%d插入后：%s\n" , i, Arrays.toString(arr));
+            System.out.println();
+        }
+    }
+}
